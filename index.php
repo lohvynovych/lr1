6e8e9ab443eeaecb713a18cd68ab34d476a36856
lohvynovych <lohvynovych1yevheniia@hneu.net>
2020-09-14 15:19:34 +0000

    update index.php - add arrays

--- index.php
+++ index.php
@@ -3,6 +3,22 @@
   <title>Тестируем PHP</title>
  </head>
  <body>
- <?php echo '<p>Привет, мир!</p>'; ?>
+ <?php 
+  echo '<p>Привет, мир!</p>'; 
+
+  echo '<p></p>';
+  echo '<p>Примеры использования массива:</p>';  
+
+  echo '<p>   - простой массив</p>';
+  $array2 = [
+    "mon" => "key",
+    "bana" => "na",
+  ];
+  var_dump($array2); 
+
+  echo '<p>   - индексированные массивы без ключа</p>';
+  $array1 = array("foo", "bar", "hallo", "world");
+  var_dump($array1);
+ ?>
  </body>
 </html>
